4a8fcb10e0e0a076774368adf2f4f7ff9b91c340
victorpaularony <smallypauls@gmail.com>
2024-06-19 15:37:07 +0300

    FILE: local package files

--- chunk/check/check.go
+++ chunk/check/check.go
@@ -2,16 +2,48 @@ package check
 
 func Chunk(slice []int, size int) [][]int {
 	var res [][]int
-	if size <= 0 {
+	if size <= 0{
 		return nil
 	}
-	for i := 0; i < len(slice); i += size {
-		stp := i + size
-		if stp > len(slice) {
-			stp = len(slice)
+	for i := 0; i <len(slice); i += size {
+		end := i + size
+		if end > len(slice) {
+			end = len(slice)
 		}
-		res = append(res, slice[i:stp])
+		res = append(res, slice[i:end])
 	}
-
 	return res
 }
+
+/*
+chunk
+Instructions
+Write a function called Chunk that receives as parameters a slice, slice []int, and a number size int. The goal of this function is to chunk a slice into many sub slices where each sub slice has the length of size.
+
+If the size is 0 it should print a newline ('\n').
+Expected function
+func Chunk(slice []int, size int) {
+
+}
+Usage
+Here is a possible program to test your function :
+
+package main
+
+func main() {
+	Chunk([]int{}, 10)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 0)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 3)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 5)
+	Chunk([]int{0, 1, 2, 3, 4, 5, 6, 7}, 4)
+}
+And its output :
+
+$ go run .
+[]
+
+[[0 1 2] [3 4 5] [6 7]]
+[[0 1 2 3 4] [5 6 7]]
+[[0 1 2 3] [4 5 6 7]]
+$
+*/
