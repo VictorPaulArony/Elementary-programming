8661da039d2b0343ec4337c42e8dec00bd781857
victorpaularony <smallypauls@gmail.com>
2024-06-19 18:08:28 +0300

    UPDATED: main.go file

--- tabmult/main.go
+++ tabmult/main.go
@@ -10,12 +10,9 @@ func main() {
 		return
 	}
 	data := os.Args[1]
-	num, _ := strconv.Atoi(data)
-
+	num, _  := strconv.Atoi(data)
+	
 	for i := 1; i <= 9; i++ {
-		// res += i * num
-		var res string
-		res += strconv.Itoa(i) + " X " + strconv.Itoa(num) + " = " + strconv.Itoa(i*num) + "\n"
-		os.Stdout.WriteString(res)
+		os.Stdout.WriteString(strconv.Itoa(i) + " x " + strconv.Itoa(num) + " = " + strconv.Itoa(num*i) + "\n")
 	}
 }
