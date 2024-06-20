2913316e37d681710f8e951e5a2ee27778462ea1
victorpaularony <smallypauls@gmail.com>
2024-06-20 12:28:16 +0300

    UPDATED: main.go file

--- ispowerof2/main.go
+++ ispowerof2/main.go
@@ -5,29 +5,36 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 2 {
+	if len(os.Args) != 2 {
 		return
 	}
-
 	data := os.Args[1]
 	num := Atoi(data)
-	// The main function checks if the number is positive and a power of 2 using the bitwise operation
-	for num > 2 {
-		if num%2 != 0 {
-			os.Stdout.WriteString("false" + "\n")
-			return
-		}
-		num /= 2
 
+	if Power(num) {
+		os.Stdout.WriteString("true\n")
+	} else {
+		os.Stdout.WriteString("false\n")
 	}
-	
-	os.Stdout.WriteString("true" + "\n")
 }
 
+// function Power to return true if a number is power of two
+func Power(num int) bool {
+	for num > 1 {
+		if num%2 != 0 {
+			return false
+		}
+		num /= 2
+	}
+	return true
+}
+
+// function Atoin to convert string to int
 func Atoi(str string) int {
-	res := 0
+	var res int
 	for _, s := range str {
 		res = res*10 + int(s-'0')
 	}
 	return res
 }
+
