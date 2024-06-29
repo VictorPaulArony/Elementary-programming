82a5cbe8139da4e356f2b8c8b4831322c3bcdef4
victorpaularony <smallypauls@gmail.com>
2024-06-29 13:46:23 +0300

    FILE: golang web task and practises

---
+++ web2/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var templates = template.Must(template.ParseFiles("index.html"))
+
+func main() {
+	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/submit", submitHandler)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func submitHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.FormValue("name")
+	data := struct {
+		Title string
+		Name  string
+	}{
+		Title: "Submission Result",
+		Name:  name,
+	}
+
+	err := templates.ExecuteTemplate(w, "index.html", data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
