aa16e2f2c529f661026dd2e805a8c364fac721a2
viarony <victorpaularony>
2024-08-19 11:55:27 +0300

    UPDATED: main.go with else function added

--- searchreplace/main.go
+++ searchreplace/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strings"
 )
 
 func main() {
@@ -12,25 +11,15 @@ func main() {
 	data := os.Args[1]
 	s := os.Args[2]
 	r := os.Args[3]
+	res := ""
 	// Replace the first occurrence of s with r in data
-	res := strings.Replace(data, s, r, -1)
+	for _, ru := range data {
+		if string(ru) == s {
+			res += r
+		} else {
+			res += string(ru)
+		}
+	}
 
 	os.Stdout.WriteString(res + "\n")
 }
-
-// Instructions
-// Write a program that takes 3 arguments, the first argument is a string in which a letter (the 2nd argument) will be replaced by another one (the 3rd argument).
-
-// If the number of arguments is different from 3, the program displays nothing.
-
-// If the second argument is not contained in the first one (the string) then the program rewrites the string followed by a newline ('\n').
-
-// Usage
-// $ go run . "hella there" "a" "o"
-// hello there
-// $ go run . "hallo thara" "a" "e"
-// hello there
-// $ go run . "abcd" "z" "l"
-// abcd
-// $ go run . "something" "a" "o" "b" "c"
-// $
