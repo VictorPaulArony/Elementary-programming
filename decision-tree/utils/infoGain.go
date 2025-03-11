0d4d73cef1c942dbfa63329020083be8781c2fde
victorpaularony <smallypauls@gmail.com>
2025-03-11 12:30:43 +0300

    FILE: utils/infoGain.go

---
+++ decision-tree/utils/infoGain.go
@@ -0,0 +1,42 @@
+package utils
+
+// function to calculate the information gain and the gain ratio
+//
+//	Information Gain = Entropy(parent) - (Weighted Average) * Entropy(child)
+func CalculateInfoGain(data *DataSet, targetIndex, targetColumn int) float64 {
+	entropyBeforeSplit := CalcutateEntropy(data, targetIndex)
+	splits := SplitData(data.Data, targetColumn)
+
+	entropyAfterSplit := 0.0
+	dataRows := len(data.Data)
+	// for GR
+	// slpitInfo := 0.0
+	for _, subset := range splits {
+		subsetDataSet := &DataSet{
+			Headers: data.Headers,
+			Data:    subset,
+		}
+		prob := float64(len(subset)) / float64(dataRows)
+		entropyAfterSplit += prob * CalcutateEntropy(subsetDataSet, targetIndex)
+
+		// for GR
+		// slpitInfo -= prob * math.Log2(prob)
+	}
+
+	return entropyBeforeSplit - entropyAfterSplit
+	// for GR
+	// return (entropyBeforeSplit - entropyAfterSplit) / slpitInfo
+}
+
+// function to split dataset int subsets based on an column/attributes
+func SplitData(data [][]string, columnIndex int) map[string][][]string {
+	// a map to hold the splits, where the key is the column value,
+	// and the value is a slice of rows that have that column value.
+	splits := make(map[string][][]string)
+
+	for _, row := range data {
+		val := row[columnIndex]
+		splits[val] = append(splits[val], row)
+	}
+	return splits
+}
