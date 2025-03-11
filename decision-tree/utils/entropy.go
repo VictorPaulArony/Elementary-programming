0d136966807ac262dbff1370118b3e1efa654ee8
victorpaularony <smallypauls@gmail.com>
2025-03-11 12:30:14 +0300

    FILE: utils/entropy.go

---
+++ decision-tree/utils/entropy.go
@@ -0,0 +1,28 @@
+package utils
+
+import "math"
+
+
+// function to salculate the entropy for the dataset
+//  for each class lable calculate probability and entropy
+func CalcutateEntropy(data *DataSet, targetIndex int) float64 {
+	var entropy float64
+	dataLen := len(data.Data)
+	if dataLen == 0 {
+		return 0
+	}
+	
+	// Create a map to count occurrences of each class label
+	countLables := make(map[string]int)
+	for _, row := range data.Data {
+		countLables[row[targetIndex]]++
+	}
+
+	// Calculate the entropy based on the class label counts
+	for _, count := range countLables {
+		prob := float64(count) / float64(dataLen)
+		// Update the entropy using the formula: -p * log2(p)
+		entropy -= prob * math.Log2(prob)
+	}
+	return entropy
+}
