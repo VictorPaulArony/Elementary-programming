31c6d4572f16afbeb600e104a67841179119c34c
victorpaularony <smallypauls@gmail.com>
2025-03-12 19:07:55 +0300

    UPDATED: to accept dynamic data

--- decision-tree/utils/gainRatio.go
+++ decision-tree/utils/gainRatio.go
@@ -1,9 +1,9 @@
 package utils
 
 // function to compute the gain ratio for the C4.5 to determine the attribute values
-func GainRatio(data *DataSet, targetIndex, targetColumn int) float64 {
+func GainRatio(data [][]string, targetIndex, targetColumn int) float64 {
 	infoGain := CalculateInfoGain(data, targetIndex, targetColumn)
-	infoSplit := SplitInformation(data.Data, targetIndex)
+	infoSplit := SplitInformation(data, targetIndex)
 
 	if infoSplit == 0 {
 		return 0
