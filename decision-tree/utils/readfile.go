5d87e2484f0532309eb09018a40d99629829a68a
victorpaularony <smallypauls@gmail.com>
2025-03-12 14:13:05 +0300

    UPDATE: function to determine if data type is continuous, categorical, or date

--- decision-tree/utils/readfile.go
+++ decision-tree/utils/readfile.go
@@ -5,12 +5,13 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"time"
 )
 
 type DataSet struct {
 	Headers  []string
 	Data     [][]string
-	DataType map[string]string // continuous or categorical
+	DataType map[string]string // continuous, categorical, data, timestamp
 	Target   string
 }
 
@@ -41,25 +42,49 @@ func ReadingFIle(fileName, targetColumn string) DataSet {
 	}
 }
 
-// function to determine if data type is continous or categorical
+// function to determine if data type is continuous, categorical, or date
 func DetermineDataType(data *DataSet) {
 	for i, header := range data.Headers {
 		if header == data.Target {
 			continue
 		}
 
-		isContinuous := true // numerical
+		isNumerical := true // numerical until proven to get category data
+		isDate := false     // date
+		isTime := false     // time
 		for _, row := range data.Data {
-			// user parse as float
-			_, err := strconv.ParseFloat(row[i], 64)
-			if err != nil {
-				isContinuous = false
-				break
+
+			// Check for missing values
+			if row[i] == "" {
+				isNumerical = false // If there's a missing value, can't be fully numerical
+				continue
 			}
+
+			// user parse as float
+			if _, err := strconv.ParseFloat(row[i], 64); err == nil {
+				isNumerical = true 
+			} else {
+				isNumerical = false
+			}
+
+			// dtermine date
+			if _, err := time.Parse("2006-01-02", row[i]); err == nil {
+				isDate = true
+			}
+
+			// determine time
+			if _, err := time.Parse(time.RFC3339, row[i]); err == nil {
+				isTime = true
+			}
+
 		}
 
-		if isContinuous {
+		if isNumerical {
 			data.DataType[header] = "continuous"
+		} else if isDate {
+			data.DataType[header] = "date"
+		} else if isTime {
+			data.DataType[header] = "time"
 		} else {
 			data.DataType[header] = "categorical"
 		}
