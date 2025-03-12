c39f0e26b5c87481f61eb07961620d93c42345af
victorpaularony <smallypauls@gmail.com>
2025-03-12 19:08:51 +0300

    TEST: function testing

--- decision-tree/main.go
+++ decision-tree/main.go
@@ -1,8 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"decisision-tree/utils"
+)
 
 // function main
 func main() {
 	fmt.Println("This is the main function")
+
+	dataset := utils.ReadingFIle("./training-data/data3.csv", "")
+
+	datatype := utils.DetermineDataType(dataset.Data, 0)
+
+	fmt.Println(datatype)
+
+	fmt.Println(dataset.Headers)
 }
