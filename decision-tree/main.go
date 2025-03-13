ce3c2cea6ee46ec653978cb848c55faf79c7d653
victorpaularony <smallypauls@gmail.com>
2025-03-13 19:28:50 +0300

    main.go

--- decision-tree/main.go
+++ decision-tree/main.go
@@ -10,11 +10,16 @@ import (
 func main() {
 	fmt.Println("This is the main function")
 
-	dataset := utils.ReadingFIle("./training-data/data3.csv", "")
+	dataset := utils.ReadingFIle("./training-data/data.csv", "PlayTennis")
 
-	datatype := utils.DetermineDataType(dataset.Data, 0)
+	datatype := utils.DetermineDataType(dataset.Data, dataset.Headers)
+
+	fmt.Println(dataset.Target)
 
 	fmt.Println(datatype)
 
-	fmt.Println(dataset.Headers)
+	tree := utils.DecisionTree(dataset.Data, dataset.Target, dataset.Headers)
+	
+
+	utils.SavePrediction("model.json", tree)
 }
