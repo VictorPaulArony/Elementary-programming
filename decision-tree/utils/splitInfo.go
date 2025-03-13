eb3ec9df5e51b42dc160646f151d0033d044bf0c
victorpaularony <smallypauls@gmail.com>
2025-03-13 12:33:31 +0300

    UPDATE: to include the numerical data set well

--- decision-tree/utils/splitInfo.go
+++ decision-tree/utils/splitInfo.go
@@ -1,22 +1,52 @@
 package utils
 
-import "math"
+import (
+	"math"
+	"sync"
+)
 
 // function to calculate the split infomation
 // measure how uniformly the data is (prevent bias)
 func SplitInformation(data [][]string, targetIndex int) float64 {
-	total := len(data)
-
-	// Create a map to count occurrences of each class label
-	columnCount := make(map[string]int)
-	for _, row := range data {
-		columnCount[row[targetIndex]]++
+	dataRows := len(data)
+	if dataRows == 0 {
+		return 0.0
 	}
 
 	splitInfo := 0.0
-	for _, count := range columnCount {
-		prob := float64(count) / float64(total)
-		splitInfo -= prob * math.Log2(prob)
+
+	colType := DetermineDataType(data, targetIndex)
+	if colType == "categorical" {
+		splits := SplitDataCategorical(data, targetIndex)
+		// goroutine to calculate the entropy
+		var wg sync.WaitGroup
+		mu := &sync.Mutex{}
+
+		for _, subset := range splits {
+			wg.Add(1)
+			go func(subset [][]string) {
+				defer wg.Done()
+
+				prob := float64(len(subset)) / float64(dataRows)
+				splitInfo -= prob * math.Log2(prob)
+
+				mu.Unlock()
+			}(subset)
+		}
+
+	}else {
+		// Handle numeric attribute
+		leftSplit, rightSplit, _ := splitByNumeric(data, targetIndex)
+		
+		if len(leftSplit) > 0 {
+			prob := float64(len(leftSplit)) / float64(dataRows)
+			splitInfo -= prob * math.Log2(prob)
+		}
+		if len(rightSplit) > 0 {
+			prob := float64(len(rightSplit)) / float64(dataRows)
+			splitInfo -= prob * math.Log2(prob)
+		}
 	}
+
 	return splitInfo
 }
