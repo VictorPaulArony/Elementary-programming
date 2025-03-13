edb665ee083c31f63b7cb2ad1fa0a90990fb183a
victorpaularony <smallypauls@gmail.com>
2025-03-13 12:30:31 +0300

    UPDATE: to check pure and common classes in data

--- decision-tree/utils/gainRatio.go
+++ decision-tree/utils/gainRatio.go
@@ -6,7 +6,56 @@ func GainRatio(data [][]string, targetIndex, targetColumn int) float64 {
 	infoSplit := SplitInformation(data, targetIndex)
 
 	if infoSplit == 0 {
-		return 0
+		return 0.0
 	}
 	return infoGain / infoSplit
+}
+
+// to find the best lable/ class to split from
+func BestLable(data [][]string, targetIndex int, lables []int) (int, float64) {
+	bestLable := -1
+	bestScore := -1.0
+
+	for _, lablecolumn := range lables {
+		score := GainRatio(data, targetIndex, lablecolumn)
+		if score > bestScore {
+			bestScore = score
+			bestLable = lablecolumn
+		}
+	}
+	return bestLable, bestScore
+}
+
+// function to determine the most common class lable
+func CommonClassLable(data [][]string, targetIndex int) string {
+	classCount := make(map[string]int)
+	mostClass := ""
+	maxCount := 0
+
+	for _, row := range data {
+		classCount[row[targetIndex]]++
+	}
+
+	for class, count := range classCount {
+		if count > maxCount {
+			maxCount = count
+			mostClass = class
+		}
+	}
+	return mostClass
+}
+
+// function to check if all the samples belong to the same calss
+func CheckPureClass(data [][]string, targetIndex int) (string, bool) {
+	if len(data) == 0 {
+		return "", true
+	}
+
+	firstClass := data[0][targetIndex]
+	for _, row := range data {
+		if row[targetIndex] != firstClass {
+			return "", false
+		}
+	}
+	return firstClass, true
 }
