f43feef249d0317fdc2306a8b8ed04f0ad55a924
victorpaularony <smallypauls@gmail.com>
2025-03-14 17:39:19 +0300

    UPDATE: goroutine to calculate

--- decision-tree/utils/splitInfo.go
+++ decision-tree/utils/splitInfo.go
@@ -7,18 +7,17 @@ import (
 
 // function to calculate the split infomation
 // measure how uniformly the data is (prevent bias)
-func SplitInformation(data [][]string, targetIndex int) float64 {
-	// targetIndex := FindColumnIndex(headers, attrName)
+func SplitInformation(data [][]string, columnIndex int) float64 {
 	dataRows := len(data)
 	if dataRows == 0 {
 		return 0.0 // prevent division by 0
 	}
 
 	splitInfo := 0.0
-	colType := DetermineDataType(data, headers)
+	colType := DetermineDataType(data, columnIndex)
 
 	if colType == "categorical" {
-		splits := splitDataCategoricalSequential(data, targetIndex)
+		splits := splitDataCategoricalParallel(data, columnIndex)
 		// goroutine to calculate the entropy
 		var wg sync.WaitGroup
 		mu := &sync.Mutex{}
@@ -27,12 +26,10 @@ func SplitInformation(data [][]string, targetIndex int) float64 {
 			wg.Add(1)
 			go func(subset [][]string) {
 				defer wg.Done()
-
 				prob := float64(len(subset)) / float64(dataRows)
 				if prob > 0 {
 					mu.Lock()
 					splitInfo -= prob * math.Log2(prob)
-
 					mu.Unlock()
 				}
 			}(subset)
@@ -40,8 +37,7 @@ func SplitInformation(data [][]string, targetIndex int) float64 {
 		wg.Wait()
 	} else {
 		// Handle numeric attribute
-		leftSplit, rightSplit, _ := SplitByNumericParallel(data, targetIndex)
-
+		leftSplit, rightSplit, _ := SplitByNumeric(data, columnIndex)
 		if len(leftSplit) > 0 {
 			prob := float64(len(leftSplit)) / float64(dataRows)
 			if prob > 0 {
@@ -54,9 +50,7 @@ func SplitInformation(data [][]string, targetIndex int) float64 {
 				splitInfo -= prob * math.Log2(prob)
 			}
 		}
-		// fmt.Println(leftSplit)
-		// fmt.Println(rightSplit)
 	}
-	// fmt.Println(splitInfo)
+
 	return splitInfo
 }
