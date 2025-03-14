bcfbb6325a55fd8f25691f0bc1ef4f55f672c2c5
victorpaularony <smallypauls@gmail.com>
2025-03-14 14:38:48 +0300

    UPDATE: THE GAINN RATION FUNCTION

--- decision-tree/utils/gainRatio.go
+++ decision-tree/utils/gainRatio.go
@@ -1,33 +1,39 @@
 package utils
 
 // function to compute the gain ratio for the C4.5 to determine the attribute values
-func GainRatio(data [][]string,attrName string, targetName string, headers []string) float64 {
+func GainRatio(data [][]string, attrName string, targetName string, headers []string) float64 {
 	infoGain := CalculateInfoGain(data, attrName, targetName, headers)
-	infoSplit := SplitInformation(data, attrName, targetName, headers)
-
+	infoSplit := SplitInformation(data, attrIndex)
 	if infoSplit == 0 {
 		return 0.0
 	}
+	// println("hello",infoGain / infoSplit)
 	return infoGain / infoSplit
 }
 
 // to find the best lable/ class to split from
-func BestLable(data [][]string, attributes []string, targetName string, headers []string) (string, float64) {
-	bestLable := ""
-	bestScore := -1.0
+func BestLable(data [][]string, attributes []string, targetName string, headers []string) (int, float64) {
+	targetIndex := FindColumnIndex(headers, targetName)
+	bestLable := -1
+	bestScore := 0.0
 
-	for _, lablecolumn := range headers {
-		score := GainRatio(data, lablecolumn, targetName, headers)
+	for lableINdex := range headers {
+		if lableINdex == targetIndex {
+			continue
+		}
+		score := GainRatio(data, targetName, targetName, headers)
 		if score > bestScore {
 			bestScore = score
-			bestLable = lablecolumn
+			bestLable = lableINdex
 		}
 	}
+	
 	return bestLable, bestScore
 }
 
 // function to determine the most common class lable
-func CommonClassLable(data [][]string, targetIndex int) string {
+func CommonClassLable(data [][]string, targetName string, headers []string) string {
+	targetIndex := FindColumnIndex(headers, targetName)
 	classCount := make(map[string]int)
 	mostClass := ""
 	maxCount := 0
@@ -46,12 +52,14 @@ func CommonClassLable(data [][]string, targetIndex int) string {
 }
 
 // function to check if all the samples belong to the same calss
-func CheckPureClass(data [][]string, targetIndex int) (string, bool) {
+func CheckPureClass(data [][]string, targetName string, headers []string) (string, bool) {
 	if len(data) == 0 {
 		return "", true
 	}
 
+	targetIndex := FindColumnIndex(headers, targetName)
 	firstClass := data[0][targetIndex]
+
 	for _, row := range data {
 		if row[targetIndex] != firstClass {
 			return "", false
